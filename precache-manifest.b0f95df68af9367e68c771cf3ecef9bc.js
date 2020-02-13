9899dcd443eebbbe67aad25ed3bc08b273528574
bandura <banduraweb@ukr.net>
2020-02-13 09:06:28 +0200

    Updates

---
+++ precache-manifest.b0f95df68af9367e68c771cf3ecef9bc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee5dc0243ac1b7f9bdfc66c8b5899fa",
+    "url": "/tbclient/index.html"
+  },
+  {
+    "revision": "119b00f03440d41361c9",
+    "url": "/tbclient/static/css/main.6e8c4cfb.chunk.css"
+  },
+  {
+    "revision": "864d053d5298a5a88d5d",
+    "url": "/tbclient/static/js/0.cc246505.chunk.js"
+  },
+  {
+    "revision": "9a4453f7d9433583318a",
+    "url": "/tbclient/static/js/1.f926f0b6.chunk.js"
+  },
+  {
+    "revision": "13661553583e4f4d9b3f",
+    "url": "/tbclient/static/js/2.ff88e18d.chunk.js"
+  },
+  {
+    "revision": "852d463634c8474aaced",
+    "url": "/tbclient/static/js/5.60e572bf.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/tbclient/static/js/5.60e572bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "119b00f03440d41361c9",
+    "url": "/tbclient/static/js/main.14aea77c.chunk.js"
+  },
+  {
+    "revision": "f78eca55f01780fbe576",
+    "url": "/tbclient/static/js/runtime-main.ce9dcbd8.js"
+  }
+]);
